61cd3cca3d50ff0ef174f88f001026b7cb0019c4
Piotr Ćwiertnia <32513700+rezt@users.noreply.github.com>
2021-09-02 13:23:49 +0200

    Add checkmarks

--- TodoListViewController.swift
+++ TodoListViewController.swift
@@ -16,6 +16,8 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
     
+    //MARK: - TableView Datasource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -25,6 +27,22 @@ class TodoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = item
         return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(tableView.cellForRow(at: indexPath)?.textLabel?.text ?? "no data")
+        
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.none {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
