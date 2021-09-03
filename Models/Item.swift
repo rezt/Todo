01296906d956b6ac131e9c3e431b74240ba76923
Piotr Ćwiertnia <32513700+rezt@users.noreply.github.com>
2021-09-03 15:13:39 +0200

    Add NSCoder functionality

---
+++ Models/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todo
+//
+//  Created by Piotr Ćwiertnia on 03/09/2021.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
--- TodoListViewController.swift
+++ TodoListViewController.swift
@@ -9,17 +9,15 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Learn swift", "???", "profit"]
+    var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
-
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        loadData()
     }
     
     //MARK: - TableView Datasource Methods
@@ -27,11 +25,14 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let item = itemArray[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = item
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
@@ -41,13 +42,14 @@ class TodoListViewController: UITableViewController {
         
         print(tableView.cellForRow(at: indexPath)?.textLabel?.text ?? "no data")
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.none {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveData()
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
     //MARK: - Add New Tasks
@@ -63,9 +65,11 @@ class TodoListViewController: UITableViewController {
                     return
                 }
                 print(item)
-                self.itemArray.append(item)
+                let newTask = Item()
+                newTask.title = item
+                self.itemArray.append(newTask)
                 
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")
+                self.saveData()
                 
                 self.tableView.reloadData()
             }
@@ -81,6 +85,29 @@ class TodoListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-
+    //MARK: - Model Manipulation Method
+    
+    func saveData() {
+        
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loadData() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
 }
 
